9e772edfa9c5e585bb99c5b87b1c9f0292d1ac76
oberdancunha <oberdancunha@gmail.com>
2021-11-13 14:56:10 -0300

    Change file name from english to portuguese.

--- flutter_drive_app/lib/external/drive/drive_memory/drive_memory_trash_data_source.dart
+++ flutter_drive_app/lib/external/drive/drive_memory/drive_memory_trash_data_source.dart
@@ -5,31 +5,31 @@ class DriveMemoryTrashDataSource extends DriveMemoryDataSource {
   DriveMemoryTrashDataSource() {
     files = [
       const DriveDto(
-        name: 'File Trash 1',
+        name: 'Arquivo descartado 1',
         extension: 'doc',
         fileSize: 4299161.6,
         createdAt: "2021-11-13 07:11:21Z",
       ),
       const DriveDto(
-        name: 'File Trash 2',
+        name: 'Arquivo descartado 2',
         extension: 'doc',
         fileSize: 102400,
         createdAt: "2021-11-12 19:35:00Z",
       ),
       const DriveDto(
-        name: 'File Trash 3',
+        name: 'Arquivo descartado 3',
         extension: 'pdf',
         fileSize: 716800,
         createdAt: "2021-11-11 21:53:10Z",
       ),
       const DriveDto(
-        name: 'File Trash 4',
+        name: 'Arquivo descartado 4',
         extension: 'image',
         fileSize: 2097152,
         createdAt: "2021-11-10 09:44:30Z",
       ),
       const DriveDto(
-        name: 'File Trash 5',
+        name: 'Arquivo descartado 5',
         extension: 'json',
         fileSize: 204800,
         createdAt: "2021-11-09 14:10:21Z",
