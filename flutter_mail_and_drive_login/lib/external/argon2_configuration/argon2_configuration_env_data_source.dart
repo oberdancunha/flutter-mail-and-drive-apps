6d4d82dc496172b5410f5daf0ea5632fb57cf09b
oberdancunha <oberdancunha@gmail.com>
2021-11-18 00:27:59 -0300

    Changes the dotEnv variable to dotEnvArgon2.

--- flutter_mail_and_drive_login/lib/external/argon2_configuration/argon2_configuration_env_data_source.dart
+++ flutter_mail_and_drive_login/lib/external/argon2_configuration/argon2_configuration_env_data_source.dart
@@ -3,15 +3,17 @@ import 'package:flutter_mail_and_drive_modules/flutter_mail_and_drive_modules.da
 import '../../infrastructure/argon2_configuration/argon2_configuration_data_source.dart';
 
 class Argon2ConfigurationEnvDataSource implements Argon2ConfigurationDataSource {
+  DotEnv dotEnvArgon2 = DotEnv();
+
   Argon2ConfigurationEnvDataSource() {
-    dotenv.load(
+    dotEnvArgon2.load(
       fileName: 'packages/flutter_mail_and_drive_login/assets/.argon2_env',
     );
   }
 
   @override
   int get bytesLenght => int.tryParse(
-        dotenv.get(
+        dotEnvArgon2.get(
           'BYTES_LENGTH',
           fallback: '1',
         ),
@@ -19,7 +21,7 @@ class Argon2ConfigurationEnvDataSource implements Argon2ConfigurationDataSource 
 
   @override
   int get parametersInteractions => int.tryParse(
-        dotenv.get(
+        dotEnvArgon2.get(
           'PARAMETERS_INTERACTIONS',
           fallback: '2',
         ),
@@ -27,21 +29,21 @@ class Argon2ConfigurationEnvDataSource implements Argon2ConfigurationDataSource 
 
   @override
   int get parametersMemoryPowerOf2 => int.tryParse(
-        dotenv.get(
+        dotEnvArgon2.get(
           'PARAMETERS_MEMORYPOWEROF2',
           fallback: '1',
         ),
       )!;
 
   @override
-  String get secretKey => dotenv.get(
+  String get secretKey => dotEnvArgon2.get(
         'SECRET_KEY',
         fallback: '',
       );
 
   @override
   int get bytesOutOff => int.tryParse(
-        dotenv.get(
+        dotEnvArgon2.get(
           'BYTES_OUT_OFF',
           fallback: '0',
         ),
