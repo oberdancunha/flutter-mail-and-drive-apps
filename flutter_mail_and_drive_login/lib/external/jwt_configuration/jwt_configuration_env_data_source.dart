dbad93126875434d017b099c0f6944d19b683e34
oberdancunha <oberdancunha@gmail.com>
2021-11-18 00:29:56 -0300

    Change the dotenv variable to dotEnvJwt.

--- flutter_mail_and_drive_login/lib/external/jwt_configuration/jwt_configuration_env_data_source.dart
+++ flutter_mail_and_drive_login/lib/external/jwt_configuration/jwt_configuration_env_data_source.dart
@@ -3,14 +3,16 @@ import 'package:flutter_mail_and_drive_modules/flutter_mail_and_drive_modules.da
 import '../../infrastructure/jwt_configuration/jwt_configuration_data_source.dart';
 
 class JwtConfigurationEnvDataSource implements JwtConfigurationDataSource {
+  DotEnv dotEnvJwt = DotEnv();
+
   JwtConfigurationEnvDataSource() {
-    dotenv.load(
+    dotEnvJwt.load(
       fileName: 'packages/flutter_mail_and_drive_login/assets/.jwt_env',
     );
   }
 
   @override
-  String get secretKey => dotenv.get(
+  String get secretKey => dotEnvJwt.get(
         'SECRET_KEY',
         fallback: '',
       );
